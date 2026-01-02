09ee0bfc52321a9731d180e00de285e62252c649
MesiAnandaPutri <mesianandaputri24@gmail.com>
2026-01-02 17:43:47 +0700

    membuat Destinasihome

---
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiHome.kt
@@ -0,0 +1,10 @@
+package com.example.myfirebase.view.route
+
+import com.example.myfirebase.R
+import com.example.myfirebase.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+}
+
