aa337dd8cb8efb3fe795064f357ce47262b8250e
MesiAnandaPutri <mesianandaputri24@gmail.com>
2026-01-09 18:20:37 +0700

    Inisialisasi EditViewModel dengan SavedStateHandle dan RepositorySiswa

--- app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/PenyediaViewModel.kt
@@ -22,6 +22,11 @@ object PenyediaViewModel {
                 aplikasiDataSiswa().container.repositorySiswa
             )
         }
-
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositorySiswa
+            )
+        }
     }
 }
