2278c3d4ead6550220bcc6a37139076db631012d
MesiAnandaPutri <mesianandaputri24@gmail.com>
2026-01-09 18:08:01 +0700

    Menyimpan perubahan data siswa ke repository jika input valid

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -42,5 +42,16 @@ RepositorySiswa
         }
     }
 
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)){
+            try {
+                repositorySiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa
+                    ())
+                println("Update Sukses: $idSiswa")
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
+        }
+    }
 
 }
