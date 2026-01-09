3fa8d75a1c629d0cdff83ba943a21ae02acd0d0b
MesiAnandaPutri <mesianandaputri24@gmail.com>
2026-01-09 18:06:48 +0700

    Memperbarui state UI sesuai perubahan input data siswa

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -30,6 +30,11 @@ RepositorySiswa
                 .toUiStateSiswa(true)
         }
     }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
+                (detailSiswa))
+    }
 
 
 }
